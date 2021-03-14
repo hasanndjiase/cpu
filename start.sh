8af83a5df3d1b75ca35b31e7ade05b6867e9922e
hasanndjiase <74407016+hasanndjiase@users.noreply.github.com>
2021-03-14 10:22:44 +0800

    run.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qxqw9kgzkfn4vjlzm3825jye4ck2xakmejzta9t.test1
 
 
 done
